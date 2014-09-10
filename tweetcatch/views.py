6cccbe3863c27effdcc7d8326d77641337207f4e
adamsimon <asimon@frameworksinstitute.org>
2014-09-09 23:25:43 -0700

    Commit test2.

--- tweetcatch/views.py
+++ tweetcatch/views.py
@@ -32,7 +32,8 @@ def home(request):
     page_name = 'Home'
     view = __name__ + '.' + page_name
     print(request.get_host())
-    print('hello!!!')
+    print('hello!!!!!')
+    print('goodbye!!!!!')
 
     template_values = return_template_values(path)
     template_values.update({'page_name': page_name.lower,
